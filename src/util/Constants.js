9133fc9e2027eebfac58d6118e330e138ddf546a
will <willmnging@gmail.com>
2023-03-08 00:51:57 -0500

    Change aws server url

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,1 +1,1 @@
-export const URLBASE_V1 = "http://truenortharithmeticback-env.eba-i9aufhm8.us-east-1.elasticbeanstalk.com/truenorth/api/v1/";
+export const URLBASE_V1 = "http://truenortharithmeticback-env-1.eba-msccvs22.us-east-1.elasticbeanstalk.com/truenorth/api/v1/";
